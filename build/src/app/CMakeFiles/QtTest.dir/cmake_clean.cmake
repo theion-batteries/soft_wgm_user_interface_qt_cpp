d1f701949abc30a4c3bf9bdb989b0f3bbacaa571
sami-theion <sami@theion.de>
2022-10-20 17:36:40 +0200

    updated build proj

---
+++ build/src/app/CMakeFiles/QtTest.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtTest_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/QtTest_autogen.dir/ParseCache.txt"
+  "QtTest_autogen"
+  "CMakeFiles/QtTest.dir/QtTest_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/QtTest.dir/QtTest_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/QtTest.dir/main.cpp.obj"
+  "CMakeFiles/QtTest.dir/main.cpp.obj.d"
+  "CMakeFiles/QtTest.dir/mainwindow.cpp.obj"
+  "CMakeFiles/QtTest.dir/mainwindow.cpp.obj.d"
+  "CMakeFiles/QtTest.dir/sensor.cpp.obj"
+  "CMakeFiles/QtTest.dir/sensor.cpp.obj.d"
+  "QtTest.exe"
+  "QtTest.exe.manifest"
+  "QtTest.pdb"
+  "libQtTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QtTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/app/CMakeFiles/QtTest_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtTest_autogen"
+  "QtTest_autogen/include/ui_mainwindow.h"
+  "QtTest_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/QtTest_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
