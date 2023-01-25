e58976a16799d961b7b75f3c4a1cf466b4328290
sami-theion <sami@theion.de>
2023-01-25 22:38:28 +0100

    updated yaml

--- cpack_install_script.bat
+++ cpack_install_script.bat
@@ -9,4 +9,5 @@ git add wgm_pkg.exe
 git add wgm_application_user_manual.pdf
 git commit -m "updated installer"
 git log --pretty=oneline -n 10
-git push https://${{ secrets.GITHUB_TOKEN }}@github.com/theion-batteries/soft_wgm_installer_update.git
+git push
+REM git push https://${{ secrets.GITHUB_TOKEN }}@github.com/theion-batteries/soft_wgm_installer_update.git
