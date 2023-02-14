0168892a6d6204a1b125940b09401a6c68b5f3ac
sami-theion <sami@theion.de>
2023-02-14 19:31:03 +0100

    added whsController tests

--- pythonTests.bat
+++ pythonTests.bat
@@ -1,3 +1,10 @@
 start "Server Axis" /B python dependencies/software_wgm_v2_cpp/dependencies/soft_wafer_holder_system_controller/tests/server_axis.py
-start "All tests" /B python dependencies/software_wgm_v2_cpp/dependencies/soft_wafer_holder_system_controller/tests/run_tests_in_order.py
+start "Server Keyence" /B python dependencies/software_wgm_v2_cpp/dependencies/soft_wafer_holder_system_controller/tests/server_keyence.py
+start "WhsMotions tests" /B python dependencies/software_wgm_v2_cpp/dependencies/soft_wafer_holder_system_controller/tests/run_whsMotionTests.py
+start "WhsKeyence tests" /B python dependencies/software_wgm_v2_cpp/dependencies/soft_wafer_holder_system_controller/tests/run_whsKeyenceTests.py
+start "WhsController tests" /B python dependencies/software_wgm_v2_cpp/dependencies/soft_wafer_holder_system_controller/tests/run_whsControllerTests.py
+
+
+
+
 REM  start "Server Keyence" /B python tests/server_keyence.py
