251bc015e90a40ea17ea4351e47de1fcb4b051de
sami-theion <sami@theion.de>
2023-02-14 12:57:28 +0100

    added pytests

--- pythonTests.bat
+++ pythonTests.bat
@@ -1,2 +1,3 @@
-start "Server Axis" /B python tests/server_axis.py
+start "Server Axis" /B python dependencies/software_wgm_v2_cpp/dependencies/soft_wafer_holder_system_controller/tests/server_axis.py
+start "All tests" /B python dependencies/software_wgm_v2_cpp/dependencies/soft_wafer_holder_system_controller/tests/run_tests_in_order.py
 REM  start "Server Keyence" /B python tests/server_keyence.py
