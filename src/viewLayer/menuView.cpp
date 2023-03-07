3bde5ea5688c2c1c69ac384e6416fff3bd1ac845
sami-theion <sami@theion.de>
2023-03-07 12:31:13 +0100

    added Process camStream

--- src/viewLayer/menuView.cpp
+++ src/viewLayer/menuView.cpp
@@ -2,11 +2,21 @@
 
 menuView::menuView(Ui::MainWindow* uiPtr)
 {
-    ui=uiPtr;
-/**************** signals and slots ********************/
- 
+    ui = uiPtr;
+    /**************** signals and slots ********************/
+         // 
+    connect(ui->actionopen_monitor_camera, &QAction::triggered, [this]() {
+        on_actionopen_monitor_camera_triggered();
+        });
 }
 
 menuView::~menuView()
 {
+}
+
+void  menuView::on_actionopen_monitor_camera_triggered()
+{
+    QString  path = "C:/Program Files/wgm_pkg/utils/camStream.exe";
+    std::cout << "starting camera stream " << std::endl;
+    QProcess::startDetached(path, QStringList());
 }
