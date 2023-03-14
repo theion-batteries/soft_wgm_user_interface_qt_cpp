5842b19356c5cb872a4ff19846887919c9f98311
saravanan-theion <saravanan@theion.de>
2023-03-14 13:50:12 +0100

    change: cpack header file

--- pkg/_CPack_Packages/win64/NSIS/wgm_pkg/include/coolingLib/ph_cooling_controller.h
+++ pkg/_CPack_Packages/win64/NSIS/wgm_pkg/include/coolingLib/ph_cooling_controller.h
@@ -13,8 +13,8 @@
 #include <iostream>
 #include <string>
 #include "sockpp/tcp_connector.h"
-#include "Windows.h" 
-#include <shellapi.h> 
+#include "Windows.h"
+#include <shellapi.h>
 #include <map>
 #include <queue>
 #include "memory.h"
@@ -27,18 +27,20 @@
 #include "meteorAdapter.h"
 #include "ph_xy_motion.h"
 #include "ph_trigger.h"
+
 class ph_cooling_controller
 {
 
 private:
     bool phCoolingControllerReady = false;
-    std::shared_ptr< meteorAdapter> ph;
-    std::shared_ptr <Iph_xy_motion> motionMover;
-    std::shared_ptr <ph_trigger> phTrigger;
+    std::shared_ptr<meteorAdapter> ph;
+    std::shared_ptr<Iph_xy_motion> motionMover;
+    std::shared_ptr<ph_trigger> phTrigger;
     double current_axis_position;
     YAML::Node config;
     cooling_config_yaml_params _ph_params;
     cooling_config_yaml_params _ph_params_default;
+
 public:
     /******* const/desctr ****/
     ph_cooling_controller();
@@ -49,15 +51,17 @@ public:
     wgm_feedbacks::enum_sub_sys_feedback ph_controller_disconnect();
     wgm_feedbacks::enum_sub_sys_feedback ph_motion_move_home();
     wgm_feedbacks::enum_sub_sys_feedback ph_motion_move_offset();
+    wgm_feedbacks::enum_sub_sys_feedback ph_motion_move_offset(const double new_pos);
     wgm_feedbacks::enum_sub_sys_feedback ph_motion_home_all();
     wgm_feedbacks::enum_sub_sys_feedback ph_motion_rotate_home();
-    wgm_feedbacks::enum_sub_sys_feedback ph_rotate_to_center(double new_pos);
-    wgm_feedbacks::enum_sub_sys_feedback ph_motion_move_to_center(double new_pos);
+    wgm_feedbacks::enum_sub_sys_feedback ph_rotate_to_center(const double degree);
+    wgm_feedbacks::enum_sub_sys_feedback ph_motion_move_to_center(const double new_pos);
     wgm_feedbacks::enum_sub_sys_feedback ph_rotate_center();
+    wgm_feedbacks::enum_sub_sys_feedback ph_rotate_to(const double degree);
     wgm_feedbacks::enum_sub_sys_feedback ph_move_center();
     wgm_feedbacks::enum_sub_sys_feedback ph_trigger_print();
     wgm_feedbacks::enum_sub_sys_feedback ph_rotate_and_print();
-    
+
     double get_center_target_distance();
     double get_rotate_to_center_param();
     /********* helper functions */
@@ -68,14 +72,13 @@ public:
     bool get_ph_cooling_controller_status();
     /*     helper getter */
     double get_axis_position();
-    double get_rotation_position();    
+    double get_rotation_position();
     double get_axis_velocity();
     double get_rotation_velocity();
-    Iph_xy_motion* get_xy_axis_ptr();
-    ph_trigger* get_trigger_ptr();
-    meteorAdapter* get_ph_ptr();
-    void sendDirectCmdSensor(std::string& cmd);
+    Iph_xy_motion *get_xy_axis_ptr();
+    ph_trigger *get_trigger_ptr();
+    meteorAdapter *get_ph_ptr();
+    void sendDirectCmdSensor(std::string &cmd);
     std::string sendDirectCmdAxis(std::string cmd);
     void reload_config_file();
-
 };
